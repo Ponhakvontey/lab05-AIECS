bd57338abfdee069efb1b2b6003b2fe402c29add
Ponhakvontey <ponhakvontey123@gmail.com>
2025-01-14 20:12:21 +0700

    Add test 1 to 5

--- src/Subject.java
+++ src/Subject.java
@@ -1,11 +1,68 @@
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-@ToString
-@NoArgsConstructor
-public class Subject {
-    private String subjectName;
+/* Name: subject.java
+Date: 14 January 2025
+Author: Dim Ponhakvontey
+Modified: 14 January 2025
+Description: Lab 05
+Functions:
+ -getPrice(): Double
+ -getProgram(): String
+ -getName(): String
+ -getCredits(): int
+ Variables:
+ -price: Double
+ -PROGRAM: String
+ -name: String
+ -credits: int */
+package week5.lab05;
+public class subject {
 
+    private static final double price;
+    static {
+        price = 9.9;
+    }
+    public static final double getPrice() {
+        return price;
+    }
+    private static final String PROGRAM = "Year 2 Program";
+
+    private String name;
+    private int credits;
+
+    
+
+    public static String getProgram() {
+        return PROGRAM;
+    }
+
+    public subject(String name, int credits) {
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Subject name cannot be null or empty.");
+        }
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        subject s = new subject("Java Programming", 5);
+        System.out.println("Subject: " + s.getName());
+        System.out.println("Credits: " + s.getCredits());
+        System.out.println("Program: " + subject.getProgram());
+        System.out.println("Price: " + subject.getPrice());
+        
+
+    }
 
 }
